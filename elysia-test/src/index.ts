a18ef02660c5178938cbfd6bdd762213dad18a0f
haneyume <6504021+haneyume@users.noreply.github.com>
2023-12-20 20:38:42 +0800

    feat: add swagger support for Elysia

--- elysia-test/src/index.ts
+++ elysia-test/src/index.ts
@@ -1,7 +1,31 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from 'elysia';
+import { html } from '@elysiajs/html';
+import { swagger } from '@elysiajs/swagger';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(html())
+  .use(
+    swagger({
+      path: '/v1/swagger',
+      documentation: {
+        info: {
+          title: 'Elysia Documentation',
+          version: '1.0.0',
+        },
+      },
+    }),
+  )
+  .get('/', () => 'Hello Elysia')
+  .get('/json', () => ({
+    hello: 'world',
+  }))
+  .post('/profile', ({ body }) => body, {
+    body: t.Object({
+      username: t.String(),
+    }),
+  })
+  .listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
